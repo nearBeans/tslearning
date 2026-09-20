1c5bf47a066179454a57d0088330d94c238d8224
nearBeans <yamaokahayata@gmail.com>
2026-09-20 13:44:23 +0900

    first commit

---
+++ main.ts
@@ -0,0 +1,79 @@
+type Status = "playing" | "paused" | "finished";
+type Genre = "action" | "rhythm" | "simulation" | "other";
+
+type GameFilter = (game: Game) => boolean;
+
+type Game = {
+    id: number;
+    title: string;
+    playHour: number;
+    status: Status;
+    genre: Genre;
+};
+
+const game: Game = {
+    id: 1,
+    title: "Minecraft",
+    playHour: 120,
+    status: "finished",
+    genre: "simulation"
+};
+
+const rhythm: Game = {
+    id: 2,
+    title: "Arcaea",
+    playHour: 520,
+    status: "paused",
+    genre: "rhythm"
+};
+
+const sns: Game = {
+    id: 3,
+    title: "VRChat",
+    playHour: 120,
+    status: "playing",
+    genre: "simulation"
+}
+
+const games: Game[] = [game, rhythm, sns];
+
+function getTitles(games: Game[]): string[] {
+    const titles: string[] = games.map((game: Game): string => {
+        return game.title;
+    });
+    return titles;
+}
+console.log(getTitles(games));
+
+function getOverHourGames(games: Game[]): Game[] {
+    const longGames: Game[] = games.filter((game: Game): boolean => {
+        return game.playHour >= 200;
+    });
+    return longGames;
+}
+console.log(getOverHourGames(games));
+
+function getOverHourTitles(games: Game[]): string[] {
+    const longTitles: string[] = games
+        .filter((game: Game): boolean => game.playHour >= 200)
+        .map((game: Game): string => game.title);
+    return longTitles;
+}
+console.log(getOverHourTitles(games));
+
+function findGameById(games: Game[], id: number): Game | undefined {
+    const result: Game | undefined = games.find((game: Game): boolean => {return game.id === id});
+    return result;
+}
+console.log(findGameById(games, 2));
+console.log(findGameById(games, 999));
+
+function showGameTitleById(games: Game[], id: number): void {
+    const result: Game | undefined = games.find((game: Game): boolean => {return game.id === id});
+    if (result !== undefined) console.log(result.title);
+}
+
+const isLongGame: GameFilter = (game: Game) => {
+    return game.playHour >= 200;
+}
+const longGames: Game[] = games.filter(isLongGame);
